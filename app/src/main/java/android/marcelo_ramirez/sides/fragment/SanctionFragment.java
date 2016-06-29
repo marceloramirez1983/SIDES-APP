ed7a293efc488d970efe5a435b716e15695dc793
Marcelo <ramirezmarcelo257@gmail.com>
2016-06-28 17:51:39 -0700

    Esqueleto de la App

---
+++ app/src/main/java/android/marcelo_ramirez/sides/fragment/SanctionFragment.java
@@ -0,0 +1,26 @@
+package android.marcelo_ramirez.sides.fragment;
+
+/**
+ * Created by gonzalopro on 6/28/16.
+ */
+
+import android.marcelo_ramirez.sides.R;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SanctionFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sanction, container, false);
+
+        return view;
+
+
+    }
+}
